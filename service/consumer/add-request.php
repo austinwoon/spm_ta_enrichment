905acd922d9b9a9dfb8843a9185e126ab9bd3d1e
Gracia Yuwono Kwantalalu <graciay.2017@smu.edu.sg>
2019-09-19 22:54:00 +0800

    Updated sql and added add-request.php

---
+++ service/consumer/add-request.php
@@ -0,0 +1,32 @@
+<html>
+    <body>        
+        <h1>Add Book</h1>
+
+        <?php
+            $title = $_POST['title'];
+            $isbn = $_POST['isbn'];
+            $author = $_POST['author'];
+            $publishYear = $_POST['publishYear'];
+
+            $json_url = curl_init("http://localhost/service/provider/add.php/");   // initiate curl 
+            $php_data = array(
+                'title' => $title, 
+                'isbn' => $isbn, 
+                'author' => $author,
+                'publishYear' => $publishYear);
+            $json_data = json_encode($php_data);
+            curl_setopt($json_url, CURLOPT_POST, 1);   // tell curl we want to do a POST request
+            curl_setopt($json_url, CURLOPT_POSTFIELDS, $json_data);    // attach json data to the curl
+            curl_setopt($json_url, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  // standard
+            curl_setopt($json_url, CURLOPT_RETURNTRANSFER, true); // return the string of curl_exec()
+            $result = curl_exec($json_url);   // execute the request
+            $result = json_decode($result, TRUE);  // decode from json to php 
+
+            if ($result["status"] == "success") {
+                header("Location:retrieve-view.php");
+            } else {
+                echo "Unsuccessful";
+            }
+        ?>
+    </body>
+</html>
--- service/consumer/add-view.php
+++ service/consumer/add-view.php
@@ -3,8 +3,7 @@
         <h1>Add Book</h1>
 
         <?php
-        if(!isset($_REQUEST['submit'])) {
-            echo "<form action='add-view.php' method='post'>";
+            echo "<form action='add-request.php' method='post'>";
                 echo "<table>
                         <tr>
                             <td> Title </td>
@@ -26,34 +25,6 @@
 
                     <input name='submit' type='submit'/>
                 </form>";
-
-        } else {
-            $title = $_POST['title'];
-            $isbn = $_POST['isbn'];
-            $author = $_POST['author'];
-            $publishYear = $_POST['publishYear'];
-
-<<<<<<< HEAD:service/add-view.php
-            $curl = Curl("post");
-
-            $json_curl = curl_init("http://localhost/service/add.php/");
-=======
-            $json_url = curl_init("http://localhost/service/provider/add.php/");   // initiate curl 
->>>>>>> ff892be7cc969fcf25c41643ed9955120d79a0fa:service/consumer/add-view.php
-            $php_data = array(
-                'title' => $title, 
-                'isbn' => $isbn, 
-                'author' => $author,
-                'publishYear' => $publishYear);
-            $json_data = json_encode($php_data);
-            curl_setopt($json_url, CURLOPT_POST, 1);   // tell curl we want to do a POST request
-            curl_setopt($json_url, CURLOPT_POSTFIELDS, $json_data);    // attach json data to the curl
-            curl_setopt($json_url, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  // standard
-            curl_setopt($json_url, CURLOPT_RETURNTRANSFER, true); // return the string of curl_exec()
-            $result = curl_exec($json_url);   // execute the request
-            $result = json_decode($result, TRUE);  // decode from json to php 
-            echo $result["status"]; // print out the result 
-        }
         ?>
     </body>
 </html>
